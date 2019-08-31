0231c0607f0a38195e8e85d4c917acdd8862e2d7
Roger Souza <rogersec42@gmail.com>
2019-08-30 23:19:39 -0300

    Add docs and specs to register

--- apps/auth/lib/auth.ex
+++ apps/auth/lib/auth.ex
@@ -1,6 +1,16 @@
 defmodule Auth do
   alias Auth.{Repo, User}
-  
+
+  @doc """
+  Register a new user
+
+  ## Example
+  ```
+  user = %{"name" => "User Name", "email" => email}
+  {:ok, _new_user} = Auth.register(user)
+  ```
+  """
+  @spec register(map()) :: {:ok, User.t()} | {:error, Ecto.Chageset.t()}
   def register(user_attrs) do
     %User{}
     |> User.changeset(user_attrs)
