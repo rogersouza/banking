9613792218afcdb0bfc4e4154547fc5b44e16754
Roger Souza <rogersec42@gmail.com>
2019-09-01 09:42:19 -0300

    Add documentation and usage examples for banking

--- apps/banking/lib/banking.ex
+++ apps/banking/lib/banking.ex
@@ -1,17 +1,40 @@
 defmodule Banking do
-  @moduledoc false
+  @moduledoc """
+  The banking system
+
+  This module holds functions for withdraws, transfers and directly credit or debit
+  of an especific user's wallet
+  """
 
   import Ecto.Query
 
   alias Banking.Repo
   alias Banking.Transaction
 
+  @type amount() :: String.t() | Money.t()
+
+  @doc """
+  This can be used to directly credit a user's wallet
+
+  ## Usage
+  ```
+  # User has balance of 0,00
+  iex> {:ok, _} = Banking.credit(user_id, "500,00")
+  iex> {:ok, balance} = Banking.balance(user_id)
+  {:ok, %Money{amount: 50000}}
+  ```
+  """
+  @spec credit(integer(), amount()) :: {:ok, Transaction.t()} | {:error, Money.t()}
   def credit(user_id, amount) do
     %Transaction{}
     |> Transaction.changeset(%{amount: amount, user_id: user_id, type: "credit"})
     |> Repo.insert()
   end
 
+  @doc """
+  Returns the user's current balance
+  """
+  @spec balance(integer()) :: Money.t()
   def balance(user_id) do
     Transaction
     |> where([t], t.user_id == ^user_id)
